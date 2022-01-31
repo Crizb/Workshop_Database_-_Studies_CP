c283ba2a61320d2f25a32045e03dea0fbeef8a13
Robert Kristof <krisrob123@gmail.com>
2022-01-31 22:32:58 +0100

    Add files via upload

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+#include <QApplication>
+#include "dataoperations.h"
+#include <stdlib.h>
+#include <crtdbg.h>
+#include <algorithm>
+
+const std::string DATABASE_LOCATION = "WorkshopDatabase.txt";
+
+#endif // MAIN_H
